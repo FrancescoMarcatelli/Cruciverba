18b43ad7db4c24bb94bae080319d7fb33bdff47f
FrancescoMarcatelli <102025217+FrancescoMarcatelli@users.noreply.github.com>
2022-03-21 19:25:44 +0100

    Creazione EF
    Ho creato le classi per il db e fatto il lavoro per l'ef

---
+++ 5I_Marcatelli_Crossword/Migrations/20220321090708_CreazioneDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _5I_Marcatelli_Crossword.Migrations
+{
+    public partial class CreazioneDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cruciverba",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Solution = table.Column<string>(type: "TEXT", nullable: true),
+                    Width = table.Column<int>(type: "INTEGER", nullable: false),
+                    Height = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cruciverba", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cruciverba");
+        }
+    }
+}
