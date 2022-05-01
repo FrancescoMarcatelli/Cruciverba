328ac494254b62f619ce6c4d65cc1c5acf967b50
FrancescoMarcatelli <102025217+FrancescoMarcatelli@users.noreply.github.com>
2022-05-01 22:28:34 +0200

    Delete 5I_MarcatelliF_Cruciverba directory

--- 5I_MarcatelliF_Cruciverba/Models/DbCruciverba.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-public class DbCruciverba : DbContext {
-    public DbSet<Cruciverba> Cruciverba { get; set; }
-    public DbSet<Suggerimento> Suggerimenti { get; set; }
-    public DbCruciverba (DbContextOptions<DbCruciverba> options) : base (options) { }
-}
