304e524a8bfb8bc9d190eda300daa60a86791835
FrancescoMarcatelli <102025217+FrancescoMarcatelli@users.noreply.github.com>
2022-05-01 22:28:44 +0200

    Delete 5I_Marcatelli_Crossword directory

--- 5I_Marcatelli_Crossword/CruciverbaDTO.cs
+++
@@ -1,8 +0,0 @@
-class CruciverbaDTO
-{
-    public string Solution {get; set;}
-    public int Width {get; set;}
-    public int Height {get; set;}
-    public List<SuggerimentoDTO> SuggerimentiOriz { get; set; }
-    public List<SuggerimentoDTO> SuggerimentiVert { get; set; }
-}
